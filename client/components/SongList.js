83198285e833d1007b51b3bdb7776565fbd79cfc
silvershade1 <silvershade1000@gmail.com>
2024-10-24 18:30:11 +0330

    feat : add delete songs Feature

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,27 +2,54 @@ import React from "react";
 import { graphql } from "react-apollo";
 import gql from "graphql-tag";
 import { Link } from "react-router";
-import QUERY from '../Queries/FetchSongs'
+import QUERY from "../Queries/FetchSongs";
 
-
-function SongList({ data: { loading, error, songs } }) {
+function SongList({ data: { loading, error, songs }, mutate }) {
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
+
+  const onSongDelete = (id) => {
+    mutate({
+      variables: { id }, 
+      refetchQueries: [{ query: QUERY }], 
+    })
+      .then(() => {
+        console.log(`Song with id ${id} deleted successfully`);
+      })
+      .catch((error) => {
+        console.error("Error deleting song:", error);
+      });
+  };
 
   return (
     <div>
       <ul className="collection">
-        {songs.map((song, index) => (
-          <li key={index} className="collection-item">
+        {songs.map((song) => (
+          <li key={song.id} className="collection-item">
             {song.title}
+            <i
+              className="material-icons"
+              style={{ cursor: "pointer", color: "red", marginLeft: "10px" }}
+              onClick={() => onSongDelete(song.id)} // ارسال id آهنگ به تابع onSongDelete
+            >
+              delete
+            </i>
           </li>
         ))}
       </ul>
-      <Link to='/song/new' className='btn-floating btn-large red right'>
-      <i className="material-icons">add</i>
+      <Link to="/song/new" className="btn-floating btn-large red right">
+        <i className="material-icons">add</i>
       </Link>
     </div>
   );
 }
 
-export default graphql(QUERY)(SongList);
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(graphql(QUERY)(SongList));
