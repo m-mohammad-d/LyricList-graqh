05aad3e502c1ca2482748d3f5f132ef57fc72c56
silvershade1 <silvershade1000@gmail.com>
2024-10-24 17:44:45 +0330

    feat : Submit music in Create song

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,27 +1,54 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
 class SongCreate extends Component {
   constructor(props) {
     super(props);
-    this.state = { title: "" }; 
+    this.state = { title: "" };
     this.handleChange = this.handleChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   handleChange(e) {
     this.setState({ title: e.target.value });
   }
 
+  onSubmit(event) {
+    event.preventDefault();
+    const { title } = this.state;
+    this.props
+      .mutate({
+        variables: { title },
+      })
+      .then(() => {
+        this.setState({ title: "" });
+      })
+      .catch((error) => {
+        console.error("Error adding song:", error);
+      });
+  }
+
   render() {
     return (
       <div>
         <h3>Create a new song</h3>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <label>Song name: </label>
           <input onChange={this.handleChange} value={this.state.title} />
+          <button type="submit">Submit</button>
         </form>
       </div>
     );
   }
 }
 
-export default SongCreate;
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      id
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate);
